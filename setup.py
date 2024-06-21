e7feadc81678791ea19aa70f8bb88a4616e6149b
masakk1 <igotscammedlmfaobutimmasak1@gmail.com>
2024-06-21 13:00:16 +0200

    Fixed setup.py - Moved to 0.1.1 to satisfy stupid testpipy

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup
 
 setup(
     name="spotzero",
-    version="0.1.0",
-    py_modules=["spotzero"],
+    version="0.1.1",
     install_requires=["dbus-python==1.3.2", "click>=8.1"],
     entry_points={
         "console_scripts": [
