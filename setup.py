243540197f610ea839ed9a64d2a419a6760167d3
Masak1 <105455722+masakk1@users.noreply.github.com>
2024-06-21 14:25:17 +0200

    Update setup.py - forgot a comma.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=["dbus-python==1.3.2", "click>=8.1"],
     description="control the spotify app with a cli or with a package",
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
     entry_points={
         "console_scripts": [
             "spotzero = spotzero.cli:cli",
